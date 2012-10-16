3070f45fee043a50cb43f3f68cd6f80bb93ea9b9
Stephen Paul Weber <singpolyma@singpolyma.net>
2012-10-16 17:02:14 -0500

    add fromURI

--- Network/URI/Partial.hs
+++ Network/URI/Partial.hs
@@ -1,4 +1,4 @@
-module Network.URI.Partial (PartialURI, relativeTo) where
+module Network.URI.Partial (PartialURI, fromURI, relativeTo) where
 
 import Network.URI (URI)
 import qualified Network.URI as URI
@@ -8,6 +8,9 @@ newtype PartialURI = PartialURI URI deriving (Show, Eq)
 
 relativeTo :: PartialURI -> URI -> URI
 relativeTo (PartialURI p) base = p `URI.relativeTo` base
+
+fromURI :: URI -> PartialURI
+fromURI = PartialURI
 
 instance IsString PartialURI where
 	fromString s = let Just u = URI.parseRelativeReference s in PartialURI u
